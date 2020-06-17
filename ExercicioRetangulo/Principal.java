b9101a00352d743418592f8fb4b3b17a4c5d4b9a
ThauanyBastos <thauanyb267@gmail.com>
2020-06-17 10:50:30 -0300

    First commit ExercicioRetangulo

---
+++ ExercicioRetangulo/Principal.java
@@ -0,0 +1,14 @@
+public class Principal {
+
+    public static void main(String args[]) {
+        
+        System.out.println("\f");
+        
+        Retangulo retangulo = new Retangulo();
+        
+        retangulo.setBase(5);
+        retangulo.setAltura(5);
+        
+        System.out.println(retangulo);
+    }
+}
